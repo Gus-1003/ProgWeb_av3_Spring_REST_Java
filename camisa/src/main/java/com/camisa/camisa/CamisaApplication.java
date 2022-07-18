d3ee92195aef34abba8e19a56ff497532614a901
GustavoMaciel <arioguto1221@hotmail.com>
2022-07-18 01:01:31 -0300

    up

---
+++ camisa/src/main/java/com/camisa/camisa/CamisaApplication.java
@@ -0,0 +1,13 @@
+package com.camisa.camisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamisaApplication.class, args);
+	}
+
+}
